b7eaca312eacc15d10c6c3b97f03e4e32a5315d2
Douglas Nicolas <dnmsmpvp@gmail.com>
2020-06-13 12:27:47 -0300

    axios

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,36 +1,39 @@
 <template>
   <section class="produtos-container">
     <div v-for="produto in produtos" :key="produto.id">
-      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo" />
-      <h2 class="titulo">{{produto.nome}}</h2>
-      <p class="preco">{{produto.preco}}</p>
-      <p class="descricao">{{produto.descricao}}</p>
+      <img
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
+        :alt="produto.fotos[0].titulo"
+      />
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p class="preco">{{ produto.preco }}</p>
+      <p class="descricao">{{ produto.descricao }}</p>
     </div>
   </section>
 </template>
 
 <script>
+import { api } from "@/services.js";
+
 export default {
   name: "ProdutosLista",
   data() {
     return {
-      produtos: null
+      produtos: null,
     };
   },
   methods: {
     async getProdutos() {
-      const response = await fetch("http://localhost:3000/produto");
-      const json = await response.json();
-
-      this.produtos = json;
-    }
+      const { data } = await api.get("produto");
+      this.produtos = data;
+    },
   },
 
   created() {
     this.getProdutos();
-  }
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
---
+++ src/services.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000"
+});
+
+// endpoint == url;
+
+export const api = {
+  get(endpoint) {
+    return axiosInstance.get(endpoint);
+  },
+
+  post(endpoint, body) {
+    return axiosInstance.post(endpoint, body)
+  }
+}
