6ce5e3d724067b964493fc3b3d0221c706541542
Douglas Nicolas <dnmsmpvp@gmail.com>
2020-06-14 21:33:13 -0300

    Deletar produtos

--- src/services.js
+++ src/services.js
@@ -13,6 +13,10 @@ export const api = {
 
   post(endpoint, body) {
     return axiosInstance.post(endpoint, body);
+  },
+
+  delete(endpoint) {
+    return axiosInstance.delete(endpoint);
   }
 }
 
